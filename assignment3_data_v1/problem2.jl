b2f17dd2d8f99e396584ab689a5a5c3c058c3801
TobyUllrich <TobyUllrich@gmail.com>
2018-12-08 23:44:01 +0100

    Sometimes H singular, becasue of Conditioning

--- assignment3_data_v1/problem2.jl
+++ assignment3_data_v1/problem2.jl
@@ -131,7 +131,7 @@ function computeransaciterations(p::Float64,k::Int,z::Float64)
   n = ( log2(1-z) ) / ( log2(1-p^k) )
   # Round up, to get atleast n iterations, and convert to Int
   n = Int(ceil(n))
-  print(n)
+  # print(n)
   return n::Int
 end
 
@@ -150,24 +150,24 @@ end
 #
 #---------------------------------------------------------
 function picksamples(points1::Array{Int,2},points2::Array{Int,2},k::Int)
-  samples1 = zeros(k,2)
-  samples2 = zeros(k,2)
-  r_old = zeroes(k)
+  sample1 = zeros(Int,k,2)
+  sample2 = zeros(Int,k,2)
+  r_old = zeros(Int,1)
   for i=1:k
     # Pick random point out of all remaining points
     r = rand(1:size(points1,1))
-    while(r == any(r_old)) # Make sure smples are not picked twice
+    while(any(r == r_old)) # Make sure smples are not picked twice
       r = rand(1:size(points1,1))
     end
     # Save the random point and the corresponding one from the other image
-    samples1[i,:] = points1[r,:]
-    samples2[i,:] = points2[r,:]
+    sample1[i,:] = points1[r,:]
+    sample2[i,:] = points2[r,:]
 
     # Save the random value to not pick it agin
-    r_old[i] = r
+    r_old = vcat(r_old,r)
   end
-
-
+  # subplot(122)
+  # PyPlot.scatter(sample2[:,1],sample2[:,2])
   @assert size(sample1) == (k,2)
   @assert size(sample2) == (k,2)
   return sample1::Array{Int,2},sample2::Array{Int,2}
@@ -188,7 +188,7 @@ end
 #---------------------------------------------------------
 function condition(points::Array{Float64,2})
   # Calculate Maximum of the Points
-  s = 0.5*maximum(abs.(points))
+  s = 0.5*maximum(sqrt.(points[:,1].^2+points[:,2].^2))
   # Calculate Mean in x direction
   tx = mean(points[:,1])
   # Calculate Mean in y direction
@@ -197,9 +197,15 @@ function condition(points::Array{Float64,2})
   T = [ 1/s 0   -tx/s ;
         0   1/s -ty/s;
         0   0   1     ]
-  points = cart2hom(points')
+  # display(T)
+  points = Common.cart2hom(points')
+
   U = T*points
-  U = U'[:,1:2]
+  points = (Common.hom2cart(points))'
+  # display(points)
+  U = (Common.hom2cart(U))'
+  U = U[:,1:2]
+  # display(U)
 
   @assert size(U) == size(points)
   @assert size(T) == (3,3)
@@ -220,8 +226,8 @@ end
 #---------------------------------------------------------
 function computehomography(points1::Array{Int,2}, points2::Array{Int,2})
   # Condition the corresponding points
-  points1,T1 = condition(points1)
-  points2,T2 = condition(points2)
+  points1,T1 = condition(Float64.(points1))
+  points2,T2 = condition(Float64.(points2))
 
   x = points1[1,1]
   y = points1[1,2]
@@ -242,10 +248,18 @@ function computehomography(points1::Array{Int,2}, points2::Array{Int,2})
   end
   # Calculate SVD from A
   U, S, V = svd(A,full=true)
+  # display(V[:,end])
   # Take last Right singular Vector to construct H
   H = [V[1:3,end]'; V[4:6,end]'; V[7:9,end]']
+  # display(H)
   # Recondition the Homography
-  H = inv(T2)*H*T1
+  # display(T1)
+  # display(inv(T2))
+  H = T2\H*T1
+  display(H)
+  display(det(H))
+
+  # display(sqrt(sum(H.^2)))
 
   @assert size(H) == (3,3)
   return H::Array{Float64,2}
@@ -266,12 +280,16 @@ end
 #
 #---------------------------------------------------------
 function computehomographydistance(H::Array{Float64,2},points1::Array{Int,2},points2::Array{Int,2})
-
-
+  # Apply homography to points1
+  inv(H)
+  points1H = Common.hom2cart(H*Common.cart2hom(points1'))'
+  points2H = Common.hom2cart(H\Common.cart2hom(points2'))'
+  # Compute Squarred difference between points in both directions
+  d2 = (points1H[:,1]-points2[:,1]).^2+(points1H[:,2]-points2[:,2]).^2+(points1[:,1]-points2H[:,1]).^2+(points1[:,2]-points2H[:,2]).^2
 
 
   @assert length(d2) == size(points1,1)
-  return d2::Array{Float64,2}
+  return d2::Array{Float64,1}#2}
 end
 
 
@@ -287,9 +305,9 @@ end
 #  indices    indices (in distance) of inliers
 #
 #---------------------------------------------------------
-function findinliers(distance::Array{Float64,2},thresh::Float64)
-
-
+function findinliers(distance::Array{Float64,1},thresh::Float64) #2}
+  indices = findall(distance.<thresh)
+  n=size(indices,1)
 
   return n::Int,indices::Array{Int,1}
 end
@@ -313,21 +331,29 @@ end
 #
 #---------------------------------------------------------
 function ransac(pairs::Array{Int,2},thresh::Float64,n::Int)
+  number_old = -1
+  bestinliers = 0
+  bestpairs = 0
+  bestH = 0
 
   # Iterate n times
   for i = 1:n
     # Pick random samples
-    samples1, samples2 = picksamples(pairs[:,1:2],pairs[:,3:4])
+    samples1, samples2 = picksamples(pairs[:,1:2],pairs[:,3:4],4)
     # Compute the Homography for the random samples
     H = computehomography(samples1,samples2)
+    # Compute the distances between tranformed points
+    d2 = computehomographydistance(H,pairs[:,1:2],pairs[:,3:4])
+    # Find the inliers
+    number, indices = findinliers(d2,thresh)
+    # If the number of inliers is greater the before save them
+    if(number>=number_old)
+      bestinliers = indices
+      bestpairs = [samples1 samples2]
+      bestH = H
+      number_old = number
+    end
   end
-
-
-
-
-
-
-
 
   @assert size(bestinliers,2) == 1
   @assert size(bestpairs) == (4,4)
@@ -416,16 +442,16 @@ function problem2()
   niterations = computeransaciterations(p,k,z)
   #
   # # apply RANSAC
-  # bestinliers,bestpairs,bestH = ransac(pairs,ransac_threshold,niterations)
-  # @printf(" # of bestinliers : %d", length(bestinliers))
+  bestinliers,bestpairs,bestH = ransac(pairs,ransac_threshold,1000)#niterations)
+  @printf(" # of bestinliers : %d", length(bestinliers))
   #
   # # show best matches
-  # showmatches(im1,im2,bestpairs)
-  # title("Best 4 Matches")
+  showmatches(im1,im2,bestpairs)
+  title("Best 4 Matches")
   #
   # # show all inliers
-  # showmatches(im1,im2,pairs[bestinliers,:])
-  # title("All Inliers")
+  showmatches(im1,im2,pairs[bestinliers,:])
+  title("All Inliers")
   #
   # # stitch images and show the result
   # showstitch(im1,im2,bestH)
@@ -436,5 +462,5 @@ function problem2()
 
   return nothing::Nothing
 end
-
+PyPlot.close("all")
 problem2()
